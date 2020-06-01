b318bd0f536e21f3f1ba15c681e886921c6be215
Adrian Rupala <adrian.rupala@whiteaster.com>
2020-06-01 21:56:49 +0200

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,5 +2,6 @@
 /app/scripts/wait_for.sh ofd_agregator_redis:6379 -t 15 -- echo "Redis (ofd_agregator_redis) is up!"
 
 python /app/manage.py buildapp --profile development
+python /app/manage.py collectstatic --noinput
 
 python /app/manage.py runserver 0.0.0.0:8000
