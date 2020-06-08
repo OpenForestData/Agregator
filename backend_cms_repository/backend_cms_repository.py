4586a93719db43c195f4e1b58bf878e64bb877d3
aleksander.nowak@whiteaster.com <aleksander.nowak@whiteaster.com>
2020-06-08 14:45:48 +0200

    Print env vars

--- backend_cms_repository/backend_cms_repository.py
+++ backend_cms_repository/backend_cms_repository.py
@@ -20,12 +20,14 @@ class BackendCmsRepository:
         """
 
         url = self.host + '/pl/cms-api/v1/facet-list'
+        print(f"URL FACET LIST: {url}")
         facet_list = requests.get(url)
         try:
             facet_list_json = json.loads(facet_list.text)
             return {field['facet_field_name']: field['facet_field_friendly_name'] for field in facet_list_json}
         except Exception:
-            return []
+            print("Did not get data form cms")
+            return {}
 
     def get_global_data(self) -> dict:
         """
