e89b21fd34443ae8678828c584903a43e74f02c3
aleksander.nowak@whiteaster.com <aleksander.nowak@whiteaster.com>
2020-06-19 15:43:48 +0200

    Filters

--- dataverse_repository/dataverse_repository.py
+++ dataverse_repository/dataverse_repository.py
@@ -65,7 +65,7 @@ class DataverseRepository:
         params['dvObjectType'] = [search_type]
 
         for key, values in params.items():
-            new_fquery = f"{key}:{' OR '.join([value for value in values])}"
+            new_fquery = f"{key}:{' OR '.join([f'*{value}*' for value in values])}"
             final_params['fq'].append(new_fquery)
         return q, final_params
 
